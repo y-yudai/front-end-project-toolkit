d3fb0334d5f2ca9759d6edeaf14f5a4073fdc757
y-yudai <yudots11@gmail.com>
2016-12-15 10:49:31 +0900

    fixed watch.js

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -6,7 +6,7 @@ var config  = require('../../gulpconfig').watch;
 
 gulp.task('watch', ['livereload'], function() {
 	gulp.watch(config.src.styles, ['styles']);
-	gulp.watch(config.src.script, ['scripts']);
+	gulp.watch(config.src.scripts, ['scripts']);
 	gulp.watch(config.src.php, ['theme']);
 	gulp.watch(config.src.ejs, ['theme']);
 	gulp.watch(config.src.noread_ejs, ['theme']);
